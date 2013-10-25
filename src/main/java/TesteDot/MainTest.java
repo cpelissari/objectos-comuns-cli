e619d6e5fa8196af572888b0db9b85da7e1d2a1b
Cristiane Iope Pelissari <cristiane.pelissari@objectos.com.br>
2013-10-25 15:02:17 -0200

    - Testes no projeto cli

---
+++ src/main/java/TesteDot/MainTest.java
@@ -0,0 +1,20 @@
+/*
+ * MainTest.java criado em 25/10/2013
+ * 
+ * Propriedade de Objectos Fábrica de Software LTDA.
+ * Reprodução parcial ou total proibida.
+ */
+package TesteDot;
+
+/**
+ * @author cristiane.pelissari@objectos.com.br (Cristiane Iope Pelissari)
+ */
+public class MainTest {
+
+  public static void main(String[] args) {
+    if (args[0].equals("--git")) {
+      System.out.println("sim");
+    }
+  }
+
+}
